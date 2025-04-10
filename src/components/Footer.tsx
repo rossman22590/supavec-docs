0ae83c46a14451a10412e71335a0cb3f0ee54f73
Ross Cohen <rosscohen1990@gmail.com>
2025-04-09 21:32:55 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -153,7 +153,7 @@ function SmallPrint() {
   return (
     <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 sm:flex-row dark:border-white/5">
       <p className="text-xs text-zinc-600 dark:text-zinc-400">
-        AI Tutor RAG &copy; Copyright {new Date().getFullYear()}. All rights
+        Supavec &copy; Copyright {new Date().getFullYear()}. All rights
         reserved.
       </p>
       <div className="flex gap-4">
@@ -161,7 +161,7 @@ function SmallPrint() {
           Follow us on X
         </SocialLink>
         <SocialLink
-          href="#
+          href="#"
           icon={GitHubIcon}
         >
           Follow us on GitHub
